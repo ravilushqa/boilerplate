7728e2eb7ac843462b88ca8e0ef51cf19b91b53a
ravilushqa <galaktionov.r@gmail.com>
2022-07-12 15:38:50 +0400

    simplify main.go

--- main.go
+++ main.go
@@ -2,73 +2,56 @@ package main
 
 import (
 	"context"
-	"time"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/neonxp/rutina"
 	_ "go.uber.org/automaxprocs"
-	"go.uber.org/dig"
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 
 	httpprovider "github.com/ravilushqa/boilerplate/providers/http"
 	loggerprovider "github.com/ravilushqa/boilerplate/providers/logger"
 )
 
-func buildContainer() (*dig.Container, error) {
-	constructors := []interface{}{
-		newConfig,
-		func(cfg *config) (*zap.Logger, error) {
-			return loggerprovider.New(cfg.Env, cfg.LogLevel)
-		},
-		func(cfg *config) *httpprovider.Server {
-			return httpprovider.New(cfg.HTTPAddress, nil)
-		},
+func main() {
+	// init dependencies
+	cfg := newConfig()
+	l, err := loggerprovider.New(cfg.Env, cfg.LogLevel)
+	if err != nil {
+		l.Fatal("failed to create logger", zap.Error(err))
+	}
+	defaultHttp := httpprovider.New(cfg.HTTPAddress, nil)
+
+	// run application
+	g, gctx := errgroup.WithContext(context.Background())
+	g.Go(func() error {
+		return listenOsSignals(gctx)
+	})
+	g.Go(func() error {
+		l.Info("starting http server")
+		return defaultHttp.Run(gctx)
+	})
+	if err := g.Wait(); err != nil {
+		l.Error("run failed", zap.Error(err))
 	}
 
-	c := dig.New()
-	for _, cf := range constructors {
-		if err := c.Provide(cf); err != nil {
-			return nil, err
-		}
-	}
-
-	return c, c.Invoke(func(h *httpprovider.Server) {})
+	// cleanup
+	defer func() {
+		_ = l.Sync() // https://github.com/uber-go/zap/issues/880
+		l.Info("graceful shutdown finished")
+	}()
+	l.Info("start gracefully shutdown...")
 }
 
-func main() {
-	tl, _ := zap.NewProduction()
-	container, err := buildContainer()
-	if err != nil {
-		tl.Fatal("cannot build depends", zap.Error(err))
-	}
-
-	r := rutina.New(rutina.WithErrChan())
-	go func() {
-		for err := range r.Errors() {
-			tl.Error("runtime error", zap.Error(err))
-		}
-	}()
-
-	err = container.Invoke(func(h *httpprovider.Server) {
-		r.Go(h.Run)
-		r.ListenOsSignals()
-	})
-	if err != nil {
-		tl.Fatal("invoke failed", zap.Error(err))
-	}
-
-	if err = r.Wait(); err != nil {
-		tl.Error("run failed", zap.Error(err))
-	}
-
-	err = container.Invoke(func(l *zap.Logger) error {
-		defer func() { _ = l.Sync() }() // https://github.com/uber-go/zap/issues/880
-		l.Info("start gracefully shutdown...")
-		_, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
+func listenOsSignals(ctx context.Context) error {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case <-ctx.Done():
 		return nil
-	})
-	if err != nil {
-		tl.Error("shutdown failed", zap.Error(err))
+	case s := <-sigCh:
+		return fmt.Errorf("received signal %s", s)
 	}
 }
